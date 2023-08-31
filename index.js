07038223b1440cfbb5b1b162abbbe0fbead71b3d
Shant Dashjian <shantgdashjian@gmail.com>
2023-08-31 21:32:24 +0400

    Update fetchReply with openai call

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-import { Configuration, OpenAIApi } from 'openai'
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-})
-
-const openai = new OpenAIApi(configuration)
-
 const chatbotConversation = document.getElementById('chatbot-conversation')
 
 let conversationStr = ''
@@ -34,33 +26,8 @@ async function fetchReply() {
   })
   const data = await response.json()
   console.log(data)
-  /*
-  Challenge:
-    1. Make a fetch request to the url using the 
-       following details. 
-       - The method should be 'POST'
-       - In the headers, the 'content-type' should 
-         be 'text/plain'
-       - The body should hold conversationStr
-    2. Save the response to a const and log it out. 
-    3. Copy and paste the updated fetchReply function 
-       to VS Code and delete any unnecessary code from 
-       index.js
-    4. Push the changes to GitHub to trigger a
-       redeploy.
-    5. Navigate to your Netlify site, hit send 
-       and see what you get in the console. (You 
-       should see "Hello World" in an object).
-  */
-  // const response = await openai.createCompletion({
-  //   model: 'davinci:ft-personal-2023-08-30-17-43-54',
-  //   prompt: conversationStr,
-  //   presence_penalty: 0,
-  //   frequency_penalty: 0.3,
-  //   max_tokens: 100,
-  //   temperature: 0,
-  //   stop: ['\n', '->']
-  // })
+  
+  
   // conversationStr += ` ${response.data.choices[0].text} \n`
   // renderTypewriterText(response.data.choices[0].text)
 }
--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -1,13 +1,25 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+import { Configuration, OpenAIApi } from 'openai'
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration)
+
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const response = await openai.createCompletion({
+      model: 'davinci:ft-personal-2023-08-30-17-43-54',
+      prompt: event.body,
+      presence_penalty: 0,
+      frequency_penalty: 0.3,
+      max_tokens: 100,
+      temperature: 0,
+      stop: ['\n', '->']
+    })
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello, there ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify({ reply: `${response.data}` }),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
